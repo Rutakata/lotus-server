f5405e0f1c9114185fbc183645c5ff7899f5fc1e
rutakata <vladpelishenko14@gmail.com>
2024-04-19 01:06:36 +0300

    Updated /images endpoint

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const { dirname } = require('path');
+const fs = require('fs');
 
 const appDir = dirname(require.main.filename);
 const app = express();
@@ -17,7 +18,31 @@ app.use(function(req, res, next) {
 });
 
 app.get('/images', (req, res) => {
-  res.send(`../../public/testing/assets/example.png`);
+  const projectName = req.query.project;
+
+  if (projectName === undefined) {
+    res.status(400).send({
+      message: 'Project name URL parameter is missing'
+    });
+  }else if (projectName === '') {
+    res.status(400).send({
+      message: 'URL parameter project is empty'
+    });
+  }else {
+    const projectDir = "D:/Lotus/client/public/" + projectName + '/assets';
+    const images = [];
+
+    fs.readdirSync(projectDir)
+      .forEach(file => {
+        const fileType = file.split('.')[1];
+
+        if (fileType === 'jpg' || fileType === 'png' || fileType === 'jpeg') {
+          images.push(`../../public/${projectName}/assets/${file}`);
+        }
+      });
+
+    res.status(200).send({images: images});
+  }  
 })
 
 app.listen(process.env.PORT || 3000)
